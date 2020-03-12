2de30ce2d7fd24beb227bec5d5c765007c449fe2
BEPUL-CSE-RU <bepul.cse.ru@gmail.com>
2020-03-12 20:10:50 +0600

    section 6 continue

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,26 @@
 import React,{Component, Fragment} from 'react';
-
 import EventDashboard from '../../feature/event/EventDashboard/EventDashboard';
 import NavBar from '../../feature/nav/NavBar/NavBar';
 import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router';
+import HomePage from '../../feature/home/HomePage'
+
 
 class App extends Component{
   render(){
     return (
       <Fragment>
         <NavBar/>
+        
         <Container className="main">
-          <EventDashboard/>
+          <Route path='/' component={HomePage}/>
+          <Route path='/events' component={EventDashboard}/>
+        
+        
+          <Route path='/events' component={EventDashboard}/>
+          <Route path='/events' component={EventDashboard}/>
+          <Route path='/events' component={EventDashboard}/>
+
         </Container>
         
       </Fragment>
